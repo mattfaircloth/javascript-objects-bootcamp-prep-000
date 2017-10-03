1b540b59eae8ca95e9409be2c0ab39215c0cb1c6
mattfaircloth <mattcfaircloth@gmail.com>
2017-10-03 00:01:48 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var playlist = {michaeljackson: 'thriller'};
 function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign({}, playlist, {[artistName] : songTitle})
 }
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName;
+}
